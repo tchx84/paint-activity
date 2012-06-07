tchx84/paint-activity

commit d20721442caa22d89f83b6c43d35d3aafae3f4bc

Target rectangle in Stamp at first usage SL #3602
Show the target rectangle inmediatly when the Stamp tool is selected and don't
remove the sensitive of the Stamp tool button if it's being used.

Signed-off-by: Manuel Kaufmann <humitos@gmail.com>

toolbox.py
@@ -349,8 +349,11 @@ class ToolsToolbarBuilder():
         self._verify_sensitive_buttons()
 
     def _verify_sensitive_buttons(self):
-        is_selected = self._activity.area.is_selected()
-        self._tool_stamp.set_sensitive(is_selected)
+        # Check if there is an area selected or if the "stamp" tool is
+        # being used
+        sensitive = self._activity.area.is_selected() or \
+            self.tool['name'] == 'stamp'
+        self._tool_stamp.set_sensitive(sensitive)
 
 
 class ButtonFillColor(ColorToolButton):

Area.py
@@ -786,6 +786,9 @@ class Area(gtk.DrawingArea):
         self.resized_stamp = self.pixbuf_stamp.scale_simple(wr, hr,
                                  gtk.gdk.INTERP_HYPER)
 
+        # Remove selected area
+        self.getout()
+
         return self.resized_stamp
 
     def undo(self):

commit 0a76f9b0ccd642cd741796b733c75afea5a0d6a7

Fixed bug instroduced in SL #3602
This patch solve the bug introduced in
d20721442caa22d89f83b6c43d35d3aafae3f4bc

Signed-off-by: Manuel Kaufmann <humitos@gmail.com>
Acked-by: Manuel Quiñones <manuq@laptop.org>

toolbox.py
@@ -352,7 +352,7 @@ class ToolsToolbarBuilder():
         # Check if there is an area selected or if the "stamp" tool is
         # being used
         sensitive = self._activity.area.is_selected() or \
-            self.tool['name'] == 'stamp'
+            self.properties['name'] == 'stamp'
         self._tool_stamp.set_sensitive(sensitive)
 
 